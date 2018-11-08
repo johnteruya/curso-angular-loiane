2f1149f993031682c661410e0e035a9a50d4d8bc
johnteruya <jteruya9@gmail.com>
2018-11-07 23:53:27 -0200

    resultado terceira aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiro2Component,
+    MeuPrimeiroComponent
   ],
   imports: [
     BrowserModule
